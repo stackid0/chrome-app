a316f50d965c7cfabddc747ab547401f757a95b7
stackid0 <73723984+stackid0@users.noreply.github.com>
2020-12-01 00:17:20 +0900

    Created Background

---
+++ background.js
@@ -0,0 +1,23 @@
+const bodyBackground = document.querySelector("body");
+
+const backgroundList = ["66023124_p0.jpg", "78236624_p0.png", "85970602_p0.png"];
+const IMG_NUMBER = 3;
+
+function paintImage(imgSrc) {
+    const image = new Image();
+    image.src = `img/${imgSrc}`;
+    bodyBackground.prepend(image);
+    image.classList.add("background-img");
+}
+
+function genRandom() {
+    const number = Math.floor(Math.random() * IMG_NUMBER);
+    return number;
+}
+
+function init() {
+    const randomSrc = backgroundList[genRandom()];
+    paintImage(randomSrc);
+}
+
+init();
